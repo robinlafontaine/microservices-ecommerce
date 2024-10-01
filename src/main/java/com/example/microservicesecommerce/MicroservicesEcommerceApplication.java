d0097cd735bbd710f947ed91b926360308382091
robinlafontaine <robin@lafontaines.eu>
2024-10-01 14:32:17 +0200

    first commit

---
+++ src/main/java/com/example/microservicesecommerce/MicroservicesEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservicesecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/microservicesecommerce/MicroservicesEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservicesecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
