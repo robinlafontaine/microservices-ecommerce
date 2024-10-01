507e6c5b913cbf601ed9d35eb3d9d03d1cdb21c3
bastub <bastub57@gmail.com>
2024-10-01 18:05:38 +0200

    Setting up auth db

--- auth/src/main/java/com/example/auth/User.java
+++ auth/src/main/java/com/example/auth/User.java
@@ -6,14 +6,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity // This tells Hibernate to make a table out of this class
-public class Users {
+public class User {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
 
     private Integer id;
     private String name;
     private String surname;
-    private String mail;
+    private String email;
     private String hash;
 
     public Integer getId() {
@@ -40,12 +40,12 @@ public class Users {
         this.surname = surname;
     }
 
-    public String getMail() {
-        return mail;
+    public String getEmail() {
+        return email;
     }
 
-    public void setMail(String mail) {
-        this.mail = mail;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     @java.lang.Override
@@ -54,7 +54,7 @@ public class Users {
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", surname='" + surname + '\'' +
-                ", mail='" + mail + '\'' +
+                ", email='" + email + '\'' +
                 '}';
     }
 
