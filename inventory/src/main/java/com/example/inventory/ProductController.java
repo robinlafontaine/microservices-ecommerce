66285b4f5c924162c246845cdf60f747b90a6bf6
Bastub <bastub57@gmail.com>
2024-10-26 11:31:30 +0200

    fixed mapping name

--- inventory/src/main/java/com/example/inventory/ProductController.java
+++ inventory/src/main/java/com/example/inventory/ProductController.java
@@ -29,7 +29,7 @@ public class ProductController {
     }
 
     // --- Read
-    @GetMapping("/blabla")
+    @GetMapping("/search")
     public ResponseEntity<List<ProductData>> searchProducts(
             @RequestParam(required = false) Long id,
             @RequestParam(required = false) String name,
